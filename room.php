3ffc8bad820fdac809efaa5bd707aa8e65111fda
ecarlson2020 <ecarlson10@web590.webfaction.com>
2018-06-25 02:46:26 +0000

    Made background styling changes to room.php

--- room.php
+++ room.php
@@ -15,25 +15,24 @@
 <video id="local-video" autoplay muted></video>
 <video id="remote-video" autoplay></video>
 <div id='chatroom'>
-	<div style='float:right;'>Time elapsed: 13 minutes</div>
-	<h2>Chatroom</h2>
+	<div style='text-align:right;'>Time elapsed: 13 minutes</div>
 	<div class='inset-box' style='background:#fff;padding:10px;'>
 		<div style='width:92%;margin-left:auto;margin-bottom:15px;'>
 			<div style='text-align:right;'>
 				<div style='display:inline-block;color:#ccc;'>3:36 PM</div>
 				<div style='display:inline-block;'>Evan Carlson</div>
 			</div>
-			<div style='padding:8px;border-radius:8px;background:#deebf7;'>This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. </div>
+			<div style='padding:8px;border-radius:4px;background:#deebf7;'>This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. This is what a chat text box will look like. </div>
 		</div>
 		<div style='width:92%;margin-right:auto;margin-bottom:15px;'>
 			<div style='text-align:right;'>
 				<div style='display:inline-block;color:#ccc;'>3:36 PM</div>
 				<div style='display:inline-block;'>Brandon Lalonde</div>
 			</div>
-			<div style='padding:8px;border-radius:8px;background:#f5dede;'>I think that is very interesting.</div>
+			<div style='padding:8px;border-radius:4px;background:#f5dede;'>I think that is very interesting.</div>
 		</div>
 	</div>
-	<textarea style='margin-top:15px;' class='inset-box' placeholder='Send message...'></textarea>
+	<textarea style='margin-top:15px;' placeholder='Send message...'></textarea>
 </div>
 <div style='position:absolute;top:50px;left:15px;z-index:1;'>Creating offer: <span id='offer'></span></div>
 <script src="js/room.js"></script>
