e017dd856eb1254debe5b47b1194d448eb2bfaf6
ecarlson2020 <ecarlson10@web590.webfaction.com>
2018-06-25 01:04:47 +0000

    Added responsive.css file. See comments at the top of the file for more information.

--- templates/headers.php
+++ templates/headers.php
@@ -4,6 +4,7 @@
 <link rel='stylesheet' href='https://evrocamedia.com/repo/font-awesome/css/font-awesome.min.css'>
 <link href="https://fonts.googleapis.com/css?family=Anaheim|Arimo|Muli|PT+Sans|Playfair+Display|Source+Sans+Pro|Tajawal|Ubuntu" rel="stylesheet">
 <link rel='stylesheet' href='css/style.css'>
+<link rel='stylesheet' href='css/responsive.css'>
 <script src='https://evrocamedia.com/repo/js/jquery-3.2.1.min.js'></script>
 <script src='https://evrocamedia.com/repo/js/functions.js'></script>
 <script src='js/local.js'></script>
