f21c9679613df249e4dc715a275c3c5cbfa598d4
ecarlson2020 <ecarlson10@web590.webfaction.com>
2018-07-10 00:40:24 +0000

    Account and chatroom style update

--- templates/body/body-account.php
+++ templates/body/body-account.php
@@ -1,6 +1,6 @@
 <?php if($_SERVER['PHP_SELF'] == '/' . $page['body'] . ".php" && $_SESSION['email']): ?>
 <div id='section5-wrap'>
-	
+	<h1 class='text-center'>My Account</h1>
 </div>
 <div id='section6-wrap'>
 	<div class='center-40'>
@@ -8,32 +8,29 @@
 		<h2>Membership & Billing</h2>
 		<div style='overflow:auto;'>
 			<div style='float:right;width:80%;'>
-				<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Update email and password</a></div>
+				<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>(Show billing history)</a></div>
 				<div class='overflow'>
 					<div style='float:right;width:80%;border-top:1px solid #ccc;padding:10px 0 5px 0;'>
 						<div>Email: <?php echo htmlspecialchars($_SESSION['email']); ?></div>
 					</div>
 				</div>
 				<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Add credit</a></div>
-				<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Contact us</a></div>
 			</div>
 		</div>
 		<h2>My Profile</h2>
-			<div style='overflow:auto;'>
-				<div style='float:right;width:80%;'>
-					<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Update email and password</a></div>
-					<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Add credit</a></div>
-					<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Contact us</a></div>
-				</div>
+		<div style='overflow:auto;'>
+			<div style='float:right;width:80%;'>
+				<div>(Profile Picture)</div>
+				<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Update email</a></div>
+				<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Change password</a></div>
 			</div>
+		</div>
 		<h2>Settings</h2>
-			<div style='overflow:auto;'>
-				<div style='float:right;width:80%;'>
-					<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Update email and password</a></div>
-					<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Add credit</a></div>
-					<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Contact us</a></div>
-				</div>
+		<div style='overflow:auto;'>
+			<div style='float:right;width:80%;'>
+				<div class='text-right' style='margin-bottom:5px;'><a href='javascript:;'>Contact us</a></div>
 			</div>
+		</div>
 		<h2>Gifts & Offers</h2>
 		<div>
 			<input type='text' placeholder='Enter code...'>
