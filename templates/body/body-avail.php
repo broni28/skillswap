6b0b3f8b6b2ad1615af26c933a023fda7eed922f
ecarlson2020 <ecarlson10@web590.webfaction.com>
2018-08-01 20:21:33 +0000

    Availability page added instructor information

--- templates/body/body-avail.php
+++ templates/body/body-avail.php
@@ -1,5 +1,92 @@
 <div id='section6-wrap'>
 	<div class='center-80'>
+		<div>
+			<img src='images/instructor-avail.jpg'>
+			<h2>Jeanne Alexandra</h2>
+			<div>$15 per hour</div>
+			<p>Jeanne has been teaching math since 2004. With over 14 years of experience, she specializes in many different areas, including math and scienece, to health and exercise. Typically, I am available on weekdays but my availability might change from day to day. It will all depend on whether or not I choose to be available. Lessons range from $15 - $20 dollars per hour, but anyone can sign up at a given time. Thanks!
+			</p>
+			<div>
+				<i class='fa fa-clock-o'></i>
+				Experience:
+				2 years
+			</div>
+			<div>
+				<i class='fa fa-diamond'></i>
+				Skill levels:
+				Intermediate
+			</div>
+			<div>
+				<i class='fa fa-drivers-license-o'></i>
+				Qualifications:
+				<div>Certified Math Teacher, Prestigious Award 2013</div>
+			</div>
+		</div>
+		<h1>Reviews</h1>
+		<div>30 reviewers</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			14
+		</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			3
+		</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			0
+		</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			0
+		</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			<i class='fa fa-star' style='opacity:0'></i>
+			0
+		</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			I thought she was a great teacher
+		</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			I thought she was okay
+		</div>
+		<div>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			<i class='fa fa-star'></i>
+			Fantastic
+		</div>
+		<a href='javascript:;' class='button'>Submit a Review</a>
+		<h1>Availability</h1>
 		<div style='padding:0 10px;cursor:pointer;'><i class='fa fa-caret-left'></i>Go Back</div>
 		<div class='overflow'>
 			<div style='float:left;'>
@@ -19,5 +106,6 @@
 				</table>
 			</div>
 		</div>
+		<a href='javascript:;' class='button'>Schedule a lesson!</a>
 	</div>
 </div>
