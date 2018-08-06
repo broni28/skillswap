b8e47ed457d027965086c828980ed396b90ab008
ecarlson2020 <ecarlson10@web590.webfaction.com>
2018-08-06 21:14:45 +0000

    Styling updates to calendar on availability page

--- templates/body/body-avail.php
+++ templates/body/body-avail.php
@@ -1,4 +1,4 @@
-<div id='section6-wrap'>
+<div id='section7-wrap'>
 	<div class='center-80'>
 		<div class='flex'>
 		<div class='box' style='width:400px;'>
@@ -26,70 +26,72 @@
 			<p>Jeanne has been teaching math since 2004. With over 14 years of experience, she specializes in many different areas, including math and scienece, to health and exercise. Typically, I am available on weekdays but my availability might change from day to day. It will all depend on whether or not I choose to be available. Lessons range from $15 - $20 dollars per hour, but anyone can sign up at a given time. Thanks!</p>
 			<div class='reviews'>
 				<h1 class='border-top'>Reviews</h1>
-				<div style='display:inline-block;'>
-					<div>30 reviewers</div>
-					<div>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						14
+				<div style='box-shadow:inset 0 0 5px #989898;padding:15px;margin-bottom:15px;'>
+					<div style='display:inline-block;'>
+						<h2>30 Reviews</h2>
+						<div>
+							<span class='small-font'>14</span>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+						</div>
+						<div>
+							<span class='small-font'>3</span>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+						</div>
+						<div>
+							<span class='small-font'>1</span>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+						</div>
+						<div>
+							<span class='small-font'>0</span>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+						</div>
+						<div>
+							<span class='small-font'>0</span>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+							<i class='fa fa-star star' style='opacity:0'></i>
+						</div>
 					</div>
-					<div>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						3
-					</div>
-					<div>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						0
-					</div>
-					<div>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						0
-					</div>
-					<div>
-						<i class='fa fa-star star'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						<i class='fa fa-star star' style='opacity:0'></i>
-						0
-					</div>
-				</div>
-				<div style='display:inline-block;'>
-					<div>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						"I thought she was a great teacher" -Bob Bilbo
-					</div>
-					<div>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						"I thought she was okay" -Shamalama Mon Doobly
-					</div>
-					<div>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						<i class='fa fa-star'></i>
-						"Fantastic" -Penningon Ahoy Matey
+					<div style='display:inline-block;vertical-align:top;margin-left:25px;'>
+						<div class='review'>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							"I thought she was a great teacher" -Bob Bilbo
+						</div>
+						<div class='review'>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							"I thought she was okay" -Shamalama Mon Doobly
+						</div>
+						<div class='review'>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							<i class='fa fa-star star'></i>
+							"Fantastic" -Penningon Ahoy Matey
+						</div>
 					</div>
 				</div>
 			</div>
@@ -98,37 +100,26 @@
 	</div>
 	<div class='box'>
 		<h1>Availability</h1>
-		<div class='overflow'>
-			<!--<div style='float:left;'>
-				<div class='text-center' style='width:223px;background:#578dc9;border:1px solid #cecece;border-bottom:1px solid #8eb3dc;color:#fff;padding:10px 0;'>
+		<div class='overflow' style="font-family:'Anaheim', sans-serif;">
+			<div style="float:left;">
+				<div class='text-center' style='background:#c1806c;color:#fff;padding:5px 0;border-left:2px solid #fff;border-right:2px solid #fff;'>
 					<div class='flex-center' style='position:relative;'>
 						<div>October, 2017</div>
 					</div>
 				</div>
-				<table style='width:225px;border:1px solid #f1f1f1;border-top:0;font-size:14px'>
-					<tr style='background:#578dc9;color:#fff;'><td>SU</td><td>MO</td><td>TU</td><td>WE</td><td>TH</td><td>FR</td><td>SA</td></tr>
+				<table>
+					<tr><th>SU</th><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th></tr>
 					<tr><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>1</td></tr>
 					<tr><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td></tr>
-					<tr><td>9</td><td>10</td><td>11</td><td style='background:#eceaea;text-align:center;border-radius:50%;font-weight:bold;'>12</td><td>13</td><td>14</td><td>15</td></tr>
+					<tr><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td><td>15</td></tr>
 					<tr><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td><td>22</td></tr>
 					<tr><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td></tr>
 					<tr><td>30</td><td>31</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
 				</table>
-			</div>-->
-			<div style='float:left;'>
-				<div class='text-center' style='background:#9dbbdc;color:#fff;padding:5px 0;'>
-					<div class='flex-center' style='position:relative;'>
-						<div>October, 2017</div>
-					</div>
-				</div>
-				<table style='width:225px;font-size:14px;background:#fafafa;font-size:10px;'>
-					<tr style='background:#578dc9;color:#fff;'><td>SU</td><td>MO</td><td>TU</td><td>WE</td><td>TH</td><td>FR</td><td>SA</td></tr>
-					<tr><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>1</td></tr>
-					<tr><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td></tr>
-					<tr><td>9</td><td>10</td><td>11</td><td style='background:#eceaea;text-align:center;border-radius:50%;font-weight:bold;'>12</td><td>13</td><td>14</td><td>15</td></tr>
-					<tr><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td><td>22</td></tr>
-					<tr><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td></tr>
-					<tr><td>30</td><td>31</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+			</div>
+			<div style='float:left;margin-left:15px;'>
+				<table>
+					
 				</table>
 			</div>
 		</div>
