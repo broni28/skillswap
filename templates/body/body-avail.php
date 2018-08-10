4740dd0822b13e1f85f05f1260c12691c1d72157
ecarlson2020 <ecarlson10@web590.webfaction.com>
2018-08-10 20:57:16 +0000

    Styling changes to avail page

--- templates/body/body-avail.php
+++ templates/body/body-avail.php
@@ -1,128 +1,127 @@
 <div id='section7-wrap'>
 	<div class='center-80'>
-		<div class='flex'>
-		<div class='box' style='width:400px;'>
-			<img src='images/instructor-avail.jpg'>
-			<h2>Jeanne Alexandra</h2>
-			<div style='padding:10px 0;'>$15 per hour</div>
-			<div class='section6-space border-top border-bottom'>
-				<i class='fa fa-clock-o'></i>
-				Experience:
-				2 years
+		<div class='overflow'>
+			<div class='box float' style='width:400px;'>
+				<img src='images/instructor-avail.jpg'>
+				<h2>Jeanne Alexandra</h2>
+				<div style='padding:10px 0;'>$15 per hour</div>
+				<div class='section6-space border-top border-bottom'>
+					<i class='fa fa-clock-o'></i>
+					Experience:
+					2 years
+				</div>
+				<div class='section6-space border-bottom'>
+					<i class='fa fa-diamond'></i>
+					Skill levels:
+					Intermediate
+				</div>
+				<div class='section6-space'>
+					<i class='fa fa-drivers-license-o'></i>
+					Qualifications:
+					Certified Math Teacher, Prestigious Award 2013
+				</div>
 			</div>
-			<div class='section6-space border-bottom'>
-				<i class='fa fa-diamond'></i>
-				Skill levels:
-				Intermediate
-			</div>
-			<div class='section6-space'>
-				<i class='fa fa-drivers-license-o'></i>
-				Qualifications:
-				Certified Math Teacher, Prestigious Award 2013
-			</div>
-		</div>
-		<div class='box' style='width:calc(100% - 400px - 100px * 2);'>
-			<h1 class='border-top'>About The Instructor</h1>
-			<p>Jeanne has been teaching math since 2004. With over 14 years of experience, she specializes in many different areas, including math and scienece, to health and exercise. Typically, I am available on weekdays but my availability might change from day to day. It will all depend on whether or not I choose to be available. Lessons range from $15 - $20 dollars per hour, but anyone can sign up at a given time. Thanks!</p>
-			<div class='reviews'>
-				<h1 class='border-top'>Reviews</h1>
-				<div style='box-shadow:inset 0 0 5px #989898;padding:15px;margin-bottom:15px;'>
-					<div style='display:inline-block;'>
-						<h2>30 Reviews</h2>
-						<div>
-							<span class='small-font'>14</span>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
+			<div class='box float' style='width:calc(100% - 400px - 100px * 2);'>
+				<h1 class='border-top margin-top'>About The Instructor</h1>
+				<p>Jeanne has been teaching math since 2004. With over 14 years of experience, she specializes in many different areas, including math and scienece, to health and exercise. Typically, I am available on weekdays but my availability might change from day to day. It will all depend on whether or not I choose to be available. Lessons range from $15 - $20 dollars per hour, but anyone can sign up at a given time. Thanks!</p>
+				<div class='reviews'>
+					<h1 class='border-top margin-top'>Reviews</h1>
+					<div style='box-shadow:inset 0 0 5px #989898;padding:15px;margin-bottom:15px;'>
+						<div style='display:inline-block;'>
+							<h2>30 Reviews</h2>
+							<div>
+								<span class='small-font'>14</span>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+							</div>
+							<div>
+								<span class='small-font'>3</span>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+							</div>
+							<div>
+								<span class='small-font'>1</span>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+							</div>
+							<div>
+								<span class='small-font'>0</span>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+							</div>
+							<div>
+								<span class='small-font'>0</span>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+								<i class='fa fa-star star' style='opacity:0'></i>
+							</div>
 						</div>
-						<div>
-							<span class='small-font'>3</span>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-						</div>
-						<div>
-							<span class='small-font'>1</span>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-						</div>
-						<div>
-							<span class='small-font'>0</span>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-						</div>
-						<div>
-							<span class='small-font'>0</span>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-							<i class='fa fa-star star' style='opacity:0'></i>
-						</div>
-					</div>
-					<div style='display:inline-block;vertical-align:top;margin-left:25px;'>
-						<div class='review'>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							"I thought she was a great teacher" -Bob Bilbo
-						</div>
-						<div class='review'>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							"I thought she was okay" -Shamalama Mon Doobly
-						</div>
-						<div class='review'>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							<i class='fa fa-star star'></i>
-							"Fantastic" -Penningon Ahoy Matey
+						<div style='display:inline-block;vertical-align:top;margin-left:25px;'>
+							<div class='review'>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								"I thought she was a great teacher" -Bob Bilbo
+							</div>
+							<div class='review'>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								"I thought she was okay" -Shamalama Mon Doobly
+							</div>
+							<div class='review'>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								<i class='fa fa-star star'></i>
+								"Fantastic" -Penningon Ahoy Matey
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<a href='javascript:;' class='button'>Submit a Review</a>
-		</div>
-	</div>
-	<div class='box'>
-		<h1>Availability</h1>
-		<div class='overflow' style="font-family:'Anaheim', sans-serif;">
-			<div style="float:left;">
-				<div class='text-center' style='background:#c1806c;color:#fff;padding:5px 0;border-left:2px solid #fff;border-right:2px solid #fff;'>
-					<div class='flex-center' style='position:relative;'>
-						<div>October, 2017</div>
+				<a href='javascript:;' class='button'>Submit a Review</a>
+				<h1 class='border-top margin-top'>Availability</h1>
+				<div class='overflow' style="font-family:'Anaheim', sans-serif;">
+					<div style="float:left;">
+						<div class='text-center' style='background:#c1806c;color:#fff;padding:5px 0;border-left:2px solid #fff;border-right:2px solid #fff;'>
+							<div class='flex-center' style='position:relative;'>
+								<div>October, 2017</div>
+							</div>
+						</div>
+						<table>
+							<tr><th>SU</th><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th></tr>
+							<tr><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>1</td></tr>
+							<tr><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td></tr>
+							<tr><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td><td>15</td></tr>
+							<tr><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td><td>22</td></tr>
+							<tr><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td></tr>
+							<tr><td>30</td><td>31</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+						</table>
+					</div>
+					<div style='float:left;margin-left:15px;'>
+						<table>
+							
+						</table>
 					</div>
 				</div>
-				<table>
-					<tr><th>SU</th><th>MO</th><th>TU</th><th>WE</th><th>TH</th><th>FR</th><th>SA</th></tr>
-					<tr><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td><td>30</td><td>1</td></tr>
-					<tr><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td></tr>
-					<tr><td>9</td><td>10</td><td>11</td><td>12</td><td>13</td><td>14</td><td>15</td></tr>
-					<tr><td>16</td><td>17</td><td>18</td><td>19</td><td>20</td><td>21</td><td>22</td></tr>
-					<tr><td>23</td><td>24</td><td>25</td><td>26</td><td>27</td><td>28</td><td>29</td></tr>
-					<tr><td>30</td><td>31</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
-				</table>
-			</div>
-			<div style='float:left;margin-left:15px;'>
-				<table>
-					
-				</table>
+				<div class='margin-top'><a href='javascript:;' class='button'>Schedule a lesson!</a></div>
 			</div>
 		</div>
-		<div><a href='javascript:;' class='button'>Schedule a lesson!</a></div>
 	</div>
 </div>
